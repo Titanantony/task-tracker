06d7191ab77e7ae72778f42b00ebad493c5406f2
Titanantony <antonytitan96@gmail.com>
2024-03-12 16:15:18 +0300

    config: including cors

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,15 @@
-const express = require( 'express' );
+var cors = require("cors");
+
+const express = require("express");
 
 const app = express();
-app.use( express.json());
+app.use(cors());
+app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send({ message: "Hello World!" }); 
-})
+app.get("/msg", (req, res) => {
+  res.send({ message: "Hello World!" });
+});
 
 app.listen(5000, () => {
-    console.log("Server is listening on port 5000...")
-})
-
-
+  console.log("Server is listening on port 5000...");
+});
