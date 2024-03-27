11ebb7b58801d93e1bc5d61bcfc7419a889ec03e
Titanantony <antonytinan96@gmail.com>
2024-03-27 15:10:34 +0300

    adding the array at the backend

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,33 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/msg", (req, res) => {
-  res.send({ message: "Hello World!" });
+// Define your array of objects
+const tasks = [
+  {
+    id: 1,
+    text: 'Doctors Appointment',
+    day: 'Feb 5th at 2:30pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Meeting at School',
+    day: 'Feb 6th at 1:30pm',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Food Shopping',
+    day: 'Feb 5th at 1:30pm',
+    reminder: false,
+  }
+];
+
+// Create a route to serve this array
+app.get("/tasks", (req, res) => {
+  res.json(tasks);
 });
+
 
 app.listen(5000, () => {
   console.log("Server is listening on port 5000...");
