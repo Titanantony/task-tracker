2a2b27c8e01a8d974fbe0a49dfc2d51afde8f51a
Titanantony <antonytinan96@gmail.com>
2024-03-27 15:11:57 +0300

    chore: communicating with server

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
 import { axiosInstance } from "./config/axios";
 import { useEffect, useState } from "react";
-import { Message } from "./components/Msg";
 import Header from "./components/Header";
-
+import Tasks from "./components/Tasks";
 function App() {
-  const [message, setMessage] = useState("No message");
+  const [tasks, setTasks] = useState({});
   useEffect(() => {
-    async function fetchMessage() {
+    async function fetchTasks() {
       try {
-        const res = await axiosInstance("http://localhost:5000/msg");
+        const res = await axiosInstance.get("http://localhost:5000/tasks");
         console.log(res.data);
-        setMessage(res.data.message);
+        setTasks(res.data);
       } catch (error) {
         console.log(error);
       }
     }
 
-    fetchMessage();
-  }, [message]);
+    fetchTasks();
+  }, []);
+    //delete task
+  const deleteTask = (id)  =>{
+    setTasks(tasks.filter((task)=> task.id !== id));
+  }
+
 
   return (
     <div className="container">
-      {/* <Message message={message} color={"green"} />
-      <Message message="Buy world" color={"blue"} /> */}
+
       <Header title="Task Tracker" />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+      ) : (
+        <p>No Tasks</p>
+      )}
     </div>
   );
 }
